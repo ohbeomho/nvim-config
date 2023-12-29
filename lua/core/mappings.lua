31170f0dd92faea49e53c4e12300e750a7cdcb3a
OhBeomho <ohbeomho0322@gmail.com>
2023-12-29 21:24:27 +0900

    highlight not move

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -2,3 +2,4 @@ vim.g.mapleader = ' '
 
 vim.keymap.set('n', '<leader>h', '<cmd>nohlsearch<cr>');
 vim.keymap.set('n', '<c-n>', '<cmd>NvimTreeToggle<cr>');
+vim.keymap.set('n', '*', '<cmd>keepjumps normal! mi*`i<cr>');
