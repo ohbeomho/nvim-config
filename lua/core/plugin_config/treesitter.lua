1f05a8382e4c62f2ca67ba704910edf63b159305
OhBeomho <ohbeomho0322@gmail.com>
2023-12-31 15:51:22 +0900

    treesitter compiler settings

--- lua/core/plugin_config/treesitter.lua
+++ lua/core/plugin_config/treesitter.lua
@@ -1,3 +1,4 @@
+require('nvim-treesitter.install').compilers = { "clang" }
 require('nvim-treesitter.configs').setup {
 	ensure_installed = { 'c', 'lua', 'javascript', 'typescript' },
 	sync_install = false,
