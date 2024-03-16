47b402d5d53444e3b2344e0e0d7bcbddc636fd31
OhBeomho <ohbeomho0322@gmail.com>
2024-03-16 13:31:40 +0900

    Plugin setting

--- lua/core/plugin_config/treesitter.lua
+++ lua/core/plugin_config/treesitter.lua
@@ -1,6 +1,6 @@
 require("nvim-treesitter.install").compilers = { "clang" }
 require("nvim-treesitter.configs").setup({
-  ensure_installed = { "c", "lua", "javascript", "typescript", "html", "css" },
+  ensure_installed = { "c", "lua", "javascript", "typescript", "html", "css", "vimdoc" },
   sync_install = false,
   highlight = {
     enable = true,
