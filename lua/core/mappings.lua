363c784fd635d3ec758f7a5911cff053044757e3
OhBeomho <ohbeomho0322@gmail.com>
2024-03-26 19:19:46 +0900

    which-key mapping

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,4 +1,5 @@
 local stylua = require("stylua-nvim")
+local wk = require("which-key")
 
 vim.g.mapleader = " "
 
@@ -25,16 +26,15 @@ local function format()
   print("Formatted")
 end
 
-vim.keymap.set("n", "<leader>h", "<cmd>nohl<cr>")
 vim.keymap.set("n", "<c-b>", "<cmd>NvimTreeToggle<cr>")
 vim.keymap.set("n", "*", "<cmd>keepjumps normal! mi*`i<cr>")
-vim.keymap.set("n", "<leader>f", format)
-vim.keymap.set("n", "<leader>k", function()
-  vim.lsp.buf.hover()
-end)
-vim.keymap.set("n", "<s-j>", "<cmd>tabnext<cr>")
-vim.keymap.set("n", "<s-k>", "<cmd>tabprevious<cr>")
 vim.keymap.set("n", "<f2>", vim.lsp.buf.rename)
-vim.keymap.set("n", "<tab>n", "<cmd>BufferNext<cr>")
-vim.keymap.set("n", "<tab>p", "<cmd>BufferPrevious<cr>")
-vim.keymap.set("n", "<tab>x", "<cmd>BufferClose<cr>")
+vim.keymap.set("n", "<s-j>", "<cmd>tabn<cr>")
+vim.keymap.set("n", "<s-k>", "<cmd>tabp<cr>")
+vim.keymap.set("n", "x", "<cmd>tabc<cr>")
+
+wk.register({
+  F = { format, "Format file" },
+  k = { vim.lsp.buf.hover, "Show definition" },
+  h = { "<cmd>nohl<cr>", "Clear highlights" },
+}, { prefix = "<leader>" })
