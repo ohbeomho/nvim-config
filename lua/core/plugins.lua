b2bf4526283520fe6eacb526910bd7c694c9ffcc
OhBeomho <ohbeomho0322@gmail.com>
2024-03-30 10:52:41 +0900

    removed some plugins

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -13,26 +13,15 @@ local packer_bootstrap = ensure_packer()
 
 return require("packer").startup(function(use)
   use("wbthomason/packer.nvim")
-
-  -- Tree view
   use("nvim-tree/nvim-tree.lua")
-
-  -- Icons
   use("nvim-tree/nvim-web-devicons")
-
-  -- Syntax highlighting
   use("nvim-treesitter/nvim-treesitter")
-
-  -- Fuzzy finder
   use({
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
     requires = { { "nvim-lua/plenary.nvim" } },
   })
-
-  -- Auto pairs
   use("cohama/lexima.vim")
-
   use({
     "kylechui/nvim-surround",
     tag = "*",
@@ -40,8 +29,6 @@ return require("packer").startup(function(use)
       require("nvim-surround").setup({})
     end,
   })
-
-  -- Theme
   use({
     "catppuccin/nvim",
     as = "catppuccin",
@@ -49,71 +36,39 @@ return require("packer").startup(function(use)
       vim.cmd([[ colorscheme catppuccin-frappe ]])
     end,
   })
-
-  -- Auto completion
   use("neovim/nvim-lspconfig")
   use("hrsh7th/cmp-nvim-lsp")
   use("hrsh7th/cmp-buffer")
   use("hrsh7th/cmp-path")
   use("hrsh7th/cmp-cmdline")
   use("hrsh7th/nvim-cmp")
-
-  -- Snippets
   use("L3MON4D3/LuaSnip")
   use("saadparwaiz1/cmp_luasnip")
   use("rafamadriz/friendly-snippets")
-
-  -- Formatting
   use("jose-elias-alvarez/null-ls.nvim")
   use("MunifTanjim/prettier.nvim")
   use("ckipp01/stylua-nvim")
-
-  -- Discord presence
   use({
     "andweeb/presence.nvim",
     config = function()
       require("presence").setup()
     end,
   })
-
-  -- Statusline
   use({
     "nvim-lualine/lualine.nvim",
     config = function()
       require("lualine").setup()
     end,
   })
-
-  -- Coding time tracker
   use("wakatime/vim-wakatime")
-
-  -- CSS color highlighting
   use({
-    "brenoprata10/nvim-highlight-colors",
+    "goolord/alpha-nvim",
+    requires = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      require("nvim-highlight-colors").setup({})
+      require("alpha").setup(require("alpha.themes.startify").config)
     end,
   })
-
-  -- Indention guide
-  use({
-    "lukas-reineke/indent-blankline.nvim",
-    config = function()
-      require("ibl").setup()
-    end,
-  })
-
-  -- Git
   use("tpope/vim-fugitive")
-
-  -- Comment highlighting
-  use({
-    "folke/todo-comments.nvim",
-    requires = { { "nvim-lua/plenary.nvim" } },
-    config = function()
-      require("todo-comments").setup()
-    end,
-  })
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
