0116018bd4bc54edc35280b8e20da8dce3c066ff
OhBeomho <ohbeomho0322@gmail.com>
2024-04-10 18:53:48 +0900

    comments

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -25,21 +25,23 @@ local function format()
   print("Formatted")
 end
 
+-- Tree
 vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<cr>")
 vim.keymap.set("n", "<leader>E", "<cmd>NvimTreeFocus<cr>")
 
+-- LSP
 vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename)
 vim.keymap.set("n", "<leader>k", vim.lsp.buf.hover)
 vim.keymap.set("n", "<leader>F", format)
 
-vim.keymap.set("n", "<leader>h", "<cmd>nohl<cr>")
+-- Terminal
+vim.keymap.set("n", "<leader>t", "<cmd>ToggleTerm<cr>")
+vim.keymap.set("t", "<esc>", "<c-\\><c-n>")
 
-vim.keymap.set("n", "<leader>t", "<cmd>ToggleTerm<cr>");
-
+-- Tabs
 vim.keymap.set("n", "<tab>n", "<cmd>tabn<cr>")
 vim.keymap.set("n", "<tab>p", "<cmd>tabp<cr>")
 vim.keymap.set("n", "<tab>x", "<cmd>tabc<cr>")
 
 vim.keymap.set("n", "*", "<cmd>keepjumps normal! mi*`i<cr>")
-
-vim.keymap.set("t", "<esc>", "<c-\\><c-n>")
+vim.keymap.set("n", "<leader>h", "<cmd>nohl<cr>")
