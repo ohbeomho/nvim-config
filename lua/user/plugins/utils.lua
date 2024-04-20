2094ddbb13db3a42a4a913499447a8c993c03a42
ohbeomho <ohbeomho0322@gmail.com>
2024-04-20 16:23:27 +0900

    a

--- lua/user/plugins/utils.lua
+++ lua/user/plugins/utils.lua
@@ -61,6 +61,7 @@ return {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
+      require("telescope").setup({})
       require("telescope").load_extension("fzf")
     end,
   },
