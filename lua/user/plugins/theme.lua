5cad30f288f766b7c904236a778ed70d2e475bca
ohbeomho <ohbeomho0322@gmail.com>
2024-04-21 11:12:02 +0900

    Disable lualine in NvimTree

--- lua/user/plugins/theme.lua
+++ lua/user/plugins/theme.lua
@@ -16,6 +16,7 @@ return {
       options = {
         component_separators = { left = "", right = "" },
         section_separators = { left = "", right = "" },
+        disabled_filetypes = { "NvimTree" },
       },
     },
   },
