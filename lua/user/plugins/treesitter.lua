cb3814b0b6311e2cee31f704c99903aaa062cb40
OhBeomho <ohbeomho0322@gmail.com>
2024-04-23 21:23:23 +0900

    cpp

--- lua/user/plugins/treesitter.lua
+++ lua/user/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
     config = function()
       require("nvim-treesitter.install").compilers = { "clang" }
       require("nvim-treesitter.configs").setup({
-        ensure_installed = { "c", "lua", "javascript", "typescript", "html", "css", "vimdoc" },
+        ensure_installed = { "c", "cpp", "lua", "javascript", "typescript", "html", "css", "vimdoc" },
         sync_install = true,
         auto_install = true,
         highlight = {
