4b123bbaffec239df93218296489dc0eaf327540
ohbeomho <ohbeomho0322@gmail.com>
2024-05-05 16:50:25 +0900

    Switched to neogit

--- lua/user/plugins/git.lua
+++ lua/user/plugins/git.lua
@@ -1,3 +1,15 @@
 return {
-  "tpope/vim-fugitive",
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "ibhagwan/fzf-lua",
+    },
+    opts = {},
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = true,
+  },
 }
