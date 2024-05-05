43be730fb925091f6050317c3efbc79b177e2444
ohbeomho <ohbeomho0322@gmail.com>
2024-05-06 08:42:41 +0900

    all mode block cursor

--- init.lua
+++ init.lua
@@ -9,7 +9,7 @@ vim.g.loaded_netrwPlugin = 1
 
 vim.opt.termguicolors = true
 
-vim.opt.guicursor = "n-v-c:block,i:hor10"
+vim.opt.guicursor = "n-v-c-i:block"
 
 vim.opt.number = true
 vim.opt.shiftwidth = 2
