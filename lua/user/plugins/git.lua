f626420c696495d0345dc74fcf31fcd6a819c50b
ohbeomho <ohbeomho0322@gmail.com>
2024-05-06 08:35:28 +0900

    Added lazygit again
    I think it is better to use it

--- lua/user/plugins/git.lua
+++ lua/user/plugins/git.lua
@@ -1,5 +1,21 @@
 return {
   {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
+  {
     "lewis6991/gitsigns.nvim",
     config = true,
   },
