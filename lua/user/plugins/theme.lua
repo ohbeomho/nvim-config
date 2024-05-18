77b58846b0f0948675fb633727b0333d3cbdfd25
ohbeomho <ohbeomho0322@gmail.com>
2024-05-18 09:32:49 +0900

    Removed indent-blankline
    It not works in my ubuntu.
    And I don't really need it

--- lua/user/plugins/theme.lua
+++ lua/user/plugins/theme.lua
@@ -34,29 +34,8 @@ return {
     end,
   },
   {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    opts = {
-      indent = {
-        char = "│",
-      },
-      scope = {
-        enabled = true,
-        show_start = false,
-        show_end = false,
-        include = {
-          node_type = {
-            lua = { "table_constructor" },
-            javascript = { "object" },
-            cpp = { "initializer_list" },
-          },
-        },
-      },
-    },
-  },
-  {
     "alvarosevilla95/luatab.nvim",
-    dependencies = { "kyazdani42/nvim-web-devicons" },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
       separator = function()
         return "│"
