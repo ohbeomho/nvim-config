4725dcc60df90192be6bba4390a5d845ff235d92
ohbeomho <ohbeomho0322@gmail.com>
2024-06-06 17:22:10 +0900

    Not needed

--- lua/user/plugins/git.lua
+++ lua/user/plugins/git.lua
@@ -1,21 +1,5 @@
 return {
   {
-    "kdheepak/lazygit.nvim",
-    cmd = {
-      "LazyGit",
-      "LazyGitConfig",
-      "LazyGitCurrentFile",
-      "LazyGitFilter",
-      "LazyGitFilterCurrentFile",
-    },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    keys = {
-      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-    },
-  },
-  {
     "lewis6991/gitsigns.nvim",
     config = true,
   },
