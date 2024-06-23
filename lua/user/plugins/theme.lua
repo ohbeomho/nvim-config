9e0d8297a7bd069a0c844048e3cfd8e697e260fb
ohbeomho <ohbeomho0322@gmail.com>
2024-06-23 17:04:52 +0900

    color highlight

--- lua/user/plugins/theme.lua
+++ lua/user/plugins/theme.lua
@@ -33,4 +33,12 @@ return {
       end,
     },
   },
+  {
+    "brenoprata10/nvim-highlight-colors",
+    config = function()
+      vim.opt.termguicolors = true
+
+      require("nvim-highlight-colors").setup({})
+    end,
+  },
 }
