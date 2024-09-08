5639dbd0fb7ca24347811b1b2f865dcdd1f2974a
ohbeomho <ohbeomho0322@gmail.com>
2024-09-08 20:28:36 +0900

    Solved "tsserver is deprecated. use ts_ls instead" issue
    idk it just happened after updating plugins
    and other people were experiencing the same thing
    and they had a solution to solve this issue
    so I just copied the code lol

--- lua/user/plugins/lsp.lua
+++ lua/user/plugins/lsp.lua
@@ -1,7 +1,7 @@
 return {
   { "williamboman/mason.nvim", lazy = false },
   "williamboman/mason-lspconfig.nvim",
-  { "folke/neodev.nvim",       config = true },
+  { "folke/neodev.nvim", config = true },
   {
     "neovim/nvim-lspconfig",
     config = function()
@@ -9,34 +9,33 @@ return {
 
       require("mason").setup()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "clangd", "tsserver", "emmet_ls" },
+        ensure_installed = { "lua_ls", "clangd", "tsserver", "emmet_language_server" },
       })
+      require("mason-lspconfig").setup_handlers({
+        function(server_name)
+          if server_name == "tsserver" then
+            server_name = "ts_ls"
+          end
 
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
+          local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      lspconfig.lua_ls.setup({
-        settings = {
-          Lua = {
-            completion = {
-              callSnippet = "Replace",
-            },
-          },
-        },
-      })
-      lspconfig.clangd.setup({
-        capabilities = capabilities,
-        on_attach = function(client)
-          client.server_capabilities.documentFormattingProvider = false
-          client.server_capabilities.documentRangeFormattingProvider = false
+          if server_name == "clang" then
+            require("lspconfig")[server_name].setup({
+              on_attach = function(client)
+                client.server_capabilities.documentFormattingProvider = false
+                client.server_capabilities.documentRangeFormattingProvider = false
+              end,
+              cmd = {
+                "clangd",
+                "--offset-encoding=utf-16",
+              },
+            })
+            return
+          end
+
+          require("lspconfig")[server_name].setup({ capabilities = capabilities })
         end,
-        cmd = {
-          "clangd",
-          "--offset-encoding=utf-16",
-        },
       })
-      lspconfig.tsserver.setup({ capabilities = capabilities })
-      lspconfig.emmet_ls.setup({ capabilities = capabilities })
     end,
   },
 }
