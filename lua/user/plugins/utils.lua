6abd9b7e217b67928753528e50fb70600caad05c
ohbeomho <ohbeomho0322@gmail.com>
2024-09-10 20:29:37 +0900

    Re-added ultimate autopair
    It is great autopair plugin

--- lua/user/plugins/utils.lua
+++ lua/user/plugins/utils.lua
@@ -106,4 +106,12 @@ return {
       harpoon:setup()
     end,
   },
+  {
+    "altermo/ultimate-autopair.nvim",
+    event = { "InsertEnter", "CmdlineEnter" },
+    branch = "v0.6", --recommended as each new version will have breaking changes
+    opts = {
+      --Config goes here
+    },
+  },
 }
