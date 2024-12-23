42ef42dcf968a9fac9df22f9071781a2f1a5dc75
ohbeomho <ohbeomho0322@gmail.com>
2024-12-23 22:03:51 +0900

    My mappings
    I think I like this

--- lua/user/plugins/cmp.lua
+++ lua/user/plugins/cmp.lua
@@ -20,7 +20,9 @@ return {
       -- see the "default configuration" section below for full documentation on how to define
       -- your own keymap.
       keymap = {
-        preset = "super-tab",
+        preset = "none",
+        ["<Tab>"] = { "accept", "fallback" },
+        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
         ["<C-j>"] = { "select_next", "fallback" },
         ["<C-k>"] = { "select_prev", "fallback" },
         ["<C-e>"] = { "cancel", "hide", "fallback" },
