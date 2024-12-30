687d9fa362d79d485c1c7efc780e8d82259cdd8c
ohbeomho <ohbeomho0322@gmail.com>
2024-12-30 21:45:54 +0900

    preselect!

--- lua/user/plugins/cmp.lua
+++ lua/user/plugins/cmp.lua
@@ -51,7 +51,7 @@ return {
       completion = {
         keyword = { range = "prefix" },
         list = {
-          selection = "auto_insert",
+          selection = "preselect",
         },
         documentation = { auto_show = true },
       },
