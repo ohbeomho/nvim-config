939dfb1577dc3d29a11c232552cae283a111da0e
ohbeomho <ohbeomho0322@gmail.com>
2025-01-13 19:13:48 +0900

    removed plugins that I don't really use

--- lua/user/plugins/utils.lua
+++ lua/user/plugins/utils.lua
@@ -14,7 +14,6 @@ return {
     config = true,
   },
   { "wakatime/vim-wakatime", lazy = false },
-  "andweeb/presence.nvim",
   {
     "anuvyklack/windows.nvim",
     dependencies = { "anuvyklack/middleclass" },
@@ -40,48 +39,9 @@ return {
     },
   },
   {
-    "folke/trouble.nvim",
-    opts = {}, -- for default options, refer to the configuration section for custom setup.
-    cmd = "Trouble",
-    keys = {
-      {
-        "<leader>xx",
-        "<cmd>Trouble diagnostics toggle<cr>",
-        desc = "Diagnostics (Trouble)",
-      },
-      {
-        "<leader>xX",
-        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-        desc = "Buffer Diagnostics (Trouble)",
-      },
-      {
-        "<leader>cs",
-        "<cmd>Trouble symbols toggle focus=false<cr>",
-        desc = "Symbols (Trouble)",
-      },
-      {
-        "<leader>cl",
-        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-        desc = "LSP Definitions / references / ... (Trouble)",
-      },
-      {
-        "<leader>xL",
-        "<cmd>Trouble loclist toggle<cr>",
-        desc = "Location List (Trouble)",
-      },
-      {
-        "<leader>xQ",
-        "<cmd>Trouble qflist toggle<cr>",
-        desc = "Quickfix List (Trouble)",
-      },
-    },
-  },
-  {
     "karb94/neoscroll.nvim",
     config = function()
-      require("neoscroll").setup({
-        hide_cursor = false,
-      })
+      require("neoscroll").setup()
     end,
   },
   {
@@ -99,11 +59,12 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
   },
   {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    config = true,
-    -- use opts = {} for passing setup options
-    -- this is equivalent to setup({}) function
+    "altermo/ultimate-autopair.nvim",
+    event = { "InsertEnter", "CmdlineEnter" },
+    branch = "v0.6", --recommended as each new version will have breaking changes
+    opts = {
+      --Config goes here
+    },
   },
   {
     "OXY2DEV/markview.nvim",
