37e685a872d15497cb1137546c54e74e0f6fe09e
ohbeomho <ohbeomho0322@gmail.com>
2025-01-14 11:00:24 +0900

    eyeliner config change
    and disabled flash S mapping in visual mode
    so I can use surround.nvim

--- lua/user/plugins/utils.lua
+++ lua/user/plugins/utils.lua
@@ -38,13 +38,20 @@ return {
     -- stylua: ignore
     keys = {
       { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-      { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+      { "S",     mode = { "n" },           function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
       { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
       { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
       { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
     },
   },
-  "jinh0/eyeliner.nvim",
+  {
+    "jinh0/eyeliner.nvim",
+    opts = {
+      highlight_on_key = true,
+      dim = true,
+      max_length = 300,
+    },
+  },
   {
     "karb94/neoscroll.nvim",
     config = function()
