018a2a098755811409437bee57ab1f774c8f9d99
ohbeomho <ohbeomho0322@gmail.com>
2025-01-18 13:07:55 +0900

    enter key to accept
    because copilot.vim using tab key already

--- lua/user/plugins/cmp.lua
+++ lua/user/plugins/cmp.lua
@@ -21,7 +21,7 @@ return {
       -- your own keymap.
       keymap = {
         preset = "none",
-        ["<Tab>"] = { "accept", "fallback" },
+        ["<Enter>"] = { "accept", "fallback" },
         ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
         ["<C-j>"] = { "select_next", "fallback" },
         ["<C-k>"] = { "select_prev", "fallback" },
