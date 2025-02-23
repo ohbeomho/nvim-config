6cb1b08ff09e42d0c1e5c0b5d1019a806cfef9a0
ohbeomho <ohbeomho0322@gmail.com>
2025-02-23 09:57:09 +0900

    [hlchunk] Add
    Nice indent, code chunk highlight

--- lua/user/plugins/theme.lua
+++ lua/user/plugins/theme.lua
@@ -34,4 +34,21 @@ return {
       require('nvim-highlight-colors').setup({})
     end,
   },
+  {
+    'shellRaining/hlchunk.nvim',
+    event = { 'BufReadPre', 'BufNewFile' },
+    config = function()
+      require('hlchunk').setup({
+        chunk = {
+          enable = true,
+        },
+        indent = {
+          enable = true,
+        },
+        line_num = {
+          enable = true,
+        },
+      })
+    end,
+  },
 }
