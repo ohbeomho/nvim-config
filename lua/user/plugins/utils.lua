11215902e34ecb2212a5bf859a4889a0ee861b5c
ohbeomho <ohbeomho0322@gmail.com>
2025-03-08 13:48:28 +0900

    [leap.nvim] Add

--- lua/user/plugins/utils.lua
+++ lua/user/plugins/utils.lua
@@ -41,6 +41,11 @@ return {
     config = true,
   },
   {
+    'ggandor/leap.nvim',
+    dependencies = { 'tpope/vim-repeat' },
+    config = function()
+      require('leap').create_default_mappings()
+    end,
   },
   {
     'karb94/neoscroll.nvim',
