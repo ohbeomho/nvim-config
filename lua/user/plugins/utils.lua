30d22bd6e504b7fa9c04f4bae9006917294cce14
ohbeomho <ohbeomho0322@gmail.com>
2025-03-08 13:48:17 +0900

    [flash.nvim] Remove

--- lua/user/plugins/utils.lua
+++ lua/user/plugins/utils.lua
@@ -41,26 +41,6 @@ return {
     config = true,
   },
   {
-    'folke/flash.nvim',
-    event = 'VeryLazy',
-    ---@type Flash.Config
-    opts = {
-      modes = {
-        char = {
-          highlight = { backdrop = false },
-          multi_line = false,
-          jump_labels = true,
-        },
-      },
-    },
-    -- stylua: ignore
-    keys = {
-      { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-      { "S",     mode = { "n" },           function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
-      { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
-      { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-      { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
-    },
   },
   {
     'karb94/neoscroll.nvim',
