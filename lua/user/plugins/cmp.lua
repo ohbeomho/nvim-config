15b3c2587762ddf39581c138fc0adf48360dfe91
ohbeomho <ohbeomho0322@gmail.com>
2025-03-15 21:17:19 +0900

    [neodev.nvim] Switch to lazydev.nvim

--- lua/user/plugins/cmp.lua
+++ lua/user/plugins/cmp.lua
@@ -36,7 +36,15 @@ return {
       -- default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, via `opts_extend`
       sources = {
-        default = { 'snippets', 'lsp', 'path', 'buffer' },
+        default = { 'snippets', 'lsp', 'path', 'buffer', 'lazydev' },
+        providers = {
+          lazydev = {
+            name = 'LazyDev',
+            module = 'lazydev.integrations.blink',
+            -- make lazydev completions top priority (see `:h blink.cmp`)
+            score_offset = 100,
+          },
+        },
         -- optionally disable cmdline completions
         -- cmdline = {},
       },
--- lua/user/plugins/lsp.lua
+++ lua/user/plugins/lsp.lua
@@ -1,13 +1,11 @@
 return {
   { 'williamboman/mason.nvim', lazy = false },
   'williamboman/mason-lspconfig.nvim',
-  { 'folke/neodev.nvim', config = true },
+  { 'folke/lazydev.nvim', ft = 'lua', opts = {} },
   {
     'neovim/nvim-lspconfig',
     dependencies = { 'saghen/blink.cmp' },
     config = function()
-      require('neodev').setup({})
-
       require('mason').setup()
       require('mason-lspconfig').setup({
         ensure_installed = { 'lua_ls', 'clangd', 'ts_ls', 'emmet_language_server', 'cssls', 'pylsp' },
