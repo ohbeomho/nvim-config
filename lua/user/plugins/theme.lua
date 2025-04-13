4681eb2fe42aa884261f9fb991ee88ad8c3de59f
ohbeomho <ohbeomho0322@gmail.com>
2025-04-13 14:26:38 +0900

    [lualine.nvim] Use default lualine config

--- lua/user/plugins/theme.lua
+++ lua/user/plugins/theme.lua
@@ -17,13 +17,7 @@ return {
 	{
 		'nvim-lualine/lualine.nvim',
 		dependencies = { 'nvim-tree/nvim-web-devicons' },
-		opts = {
-			options = {
-				theme = 'auto',
-				component_separators = { left = '', right = '' },
-				section_separators = { left = '', right = '' },
-			},
-		},
+		opts = {},
 	},
 	{
 		'alvarosevilla95/luatab.nvim',
