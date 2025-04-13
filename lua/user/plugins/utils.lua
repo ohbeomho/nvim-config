93dd8fac7061ada3b12e37e0e017f9ee26682ff7
ohbeomho <ohbeomho0322@gmail.com>
2025-04-13 14:36:51 +0900

    [outline.nvim] Remove
    I don't need it since I have trouble.nvim

--- lua/user/plugins/utils.lua
+++ lua/user/plugins/utils.lua
@@ -109,17 +109,6 @@ return {
 		end,
 	},
 	{
-		'hedyhli/outline.nvim',
-		config = function()
-			-- Example mapping to toggle outline
-			vim.keymap.set('n', '<leader>o', '<cmd>Outline<CR>', { desc = 'Toggle Outline' })
-
-			require('outline').setup({
-				-- Your setup opts here (leave empty to use defaults)
-			})
-		end,
-	},
-	{
 		'folke/trouble.nvim',
 		opts = {},
 		cmd = 'Trouble',
