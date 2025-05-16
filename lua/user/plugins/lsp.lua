7bb7fe03e09e2a779cae2ea74c4f251e253278be
ohbeomho <ohbeomho0322@gmail.com>
2025-05-16 21:38:02 +0900

    [mason-lspconfig] lsp config change

--- lua/user/plugins/lsp.lua
+++ lua/user/plugins/lsp.lua
@@ -9,28 +9,7 @@ return {
 			require('mason').setup()
 			require('mason-lspconfig').setup({
 				ensure_installed = { 'lua_ls', 'clangd', 'ts_ls', 'emmet_language_server', 'cssls', 'pylsp' },
-				automatic_installation = true,
-			})
-			require('mason-lspconfig').setup_handlers({
-				function(server_name)
-					local capabilities = require('blink.cmp').get_lsp_capabilities()
-					require('lspconfig')[server_name].setup({ capabilities = capabilities })
-				end,
-				['clangd'] = function()
-					local capabilities = require('blink.cmp').get_lsp_capabilities()
-
-					require('lspconfig').clangd.setup({
-						capabilities = capabilities,
-						on_attach = function(client)
-							client.server_capabilities.documentFormattingProvider = false
-							client.server_capabilities.documentRangeFormattingProvider = false
-						end,
-						cmd = {
-							'clangd',
-							'--offset-encoding=utf-16',
-						},
-					})
-				end,
+				automatic_enable = true,
 			})
 		end,
 	},
