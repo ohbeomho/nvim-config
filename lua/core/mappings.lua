8234327426e9173713c1e52d4d659b819c1fed6f
ohbeomho <ohbeomho0322@gmail.com>
2025-06-08 13:32:59 +0900

    Remove leap plugin keymap
    I forgot to remove this lol

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -29,8 +29,6 @@ vim.keymap.set('n', '<C-w>=', '<cmd>WindowsEqualize<cr>')
 -- Marks
 vim.keymap.set('n', '<leader>dm', '<cmd>delm! | delm A-Z0-9<cr>')
 
--- Leap
-vim.keymap.set({ 'n', 'x' }, 's', '<Plug>(leap)')
 
 -- Others
 vim.keymap.set('n', '*', '<cmd>keepjumps normal! mi*`i<cr>')
