7c166a9dd8fdba18259193c257aa0c22a88b4a00
ohbeomho <ohbeomho0322@gmail.com>
2025-06-16 23:10:08 +0900

    [mason-lspconfig] ensure gopls is installed

--- lua/user/plugins/lsp.lua
+++ lua/user/plugins/lsp.lua
@@ -8,7 +8,7 @@ return {
 		config = function()
 			require('mason').setup()
 			require('mason-lspconfig').setup({
-				ensure_installed = { 'lua_ls', 'clangd', 'ts_ls', 'emmet_language_server', 'cssls', 'pylsp' },
+				ensure_installed = { 'lua_ls', 'clangd', 'ts_ls', 'emmet_language_server', 'cssls', 'pylsp', 'gopls' },
 				automatic_enable = true,
 			})
 		end,
