98cd9aeb01318c8eafe9126139d7332e89b3c2fe
ohbeomho <ohbeomho0322@gmail.com>
2025-07-02 21:31:43 +0900

    [lspsaga.nvim] disable light bulb

--- lua/user/plugins/lsp.lua
+++ lua/user/plugins/lsp.lua
@@ -16,7 +16,11 @@ return {
 	{
 		'nvimdev/lspsaga.nvim',
 		config = function()
-			require('lspsaga').setup({})
+			require('lspsaga').setup({
+				ui = {
+					code_action = '',
+				},
+			})
 		end,
 		dependencies = {
 			'nvim-treesitter/nvim-treesitter', -- optional
