ee62c61bd5b04c774a950e330f495d8208fb8e29
ohbeomho <ohbeomho0322@gmail.com>
2025-07-17 17:05:21 +0900

    [fFtT-highlights.nvim] Highlight when using f/F/t/T
    It is awesome!

--- lua/user/plugins/utils.lua
+++ lua/user/plugins/utils.lua
@@ -99,4 +99,14 @@ return {
 			{ '<leader>u', "<cmd>lua require('undotree').toggle()<cr>" },
 		},
 	},
+	{
+		'samiulsami/fFtT-highlights.nvim',
+		config = function()
+			---@module "fFtT-highlights"
+			---@type fFtT_highlights.opts
+			require('fFtT-highlights'):setup({
+				---See below for default configuration options
+			})
+		end,
+	},
 }
